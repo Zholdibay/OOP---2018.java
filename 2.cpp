5b736058edcbc8f8e1c85a8fa749e6ca5b306d0d
unknown <jorikB_99@mail.ru>
2018-09-04 13:08:13 +0300

    Jorik

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int n;
+cin >> n;
+int value = 1;
+int curSqr = value*value;
+
+while(curSqr <= n){h
+	cout << curSqr << ' ';
+	value++;
+	curSqr = value*value;
+}asdasdas
+
+
+	return 0;
+}
